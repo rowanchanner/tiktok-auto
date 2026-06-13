e1e3c14812c48429977de9a6055230d2a042cfcb
Rowan <rowanchanner@gmail.com>
2026-06-13 22:25:45 +0100

    Activate virtual environment in run script

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Activate the virtual environment where all the packages were installed
+source /opt/venv/bin/activate
+
 # Start a dummy web server on the port Railway provides so it doesn't kill the app
 PORT=${PORT:-8080}
 python -m http.server $PORT &
